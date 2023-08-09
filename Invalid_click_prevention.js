15b1eb5683c938c158e7717aa0bb245cc18f95bc
seungm2n <110989761+seungm2n@users.noreply.github.com>
2023-08-09 21:48:50 +0900

    Update Invalid_click_prevention.js

--- Invalid_click_prevention.js
+++ Invalid_click_prevention.js
@@ -1,37 +1,36 @@
-const clickLimit = 3;
-const timeLimit = 1111000;
-const accessLimit = 1000; 
-let clickCount = 0;
-let lastClickTime = 0;
+  const clickLimit = 3;
+  const timeLimit = 111000;
+  const accessLimit = 5000; 
+  let clickCount = 0;
+  let lastClickTime = 0;
 
-const adsDiv = document.querySelector('.ultp-heading-inner');
+  const adsDiv = document.querySelector('.ultp-heading-inner');
 
-  adsDiv.addEventListener('click', () => {
+    adsDiv.addEventListener('click', () => {
+    const currentTime = Date.now();
+    
+    if (currentTime - lastClickTime <= timeLimit) {
+      clickCount++;
+
+      if (clickCount >= clickLimit) {
+        alert("부정클릭이 확인되었습니다.");
+     adsDiv.style.pointerEvents = "none"; 
+     location.reload();
+      }
+    } else {
+      clickCount = 1;
+        adsDiv.style.pointerEvents = "auto";
+    }
+
+    lastClickTime = currentTime;
+  });
+
+  // 페이지 접근 제한
+  const lastAccessTime = localStorage.getItem('lastAccessTime');
   const currentTime = Date.now();
   
-  if (currentTime - lastClickTime <= timeLimit) {
-    clickCount++;
-
-    if (clickCount >= clickLimit) {
-   adsDiv.innerHTML = "클릭 제한 초과";
-   adsDiv.style.pointerEvents = "none"; 
-   location.reload();
-  // 페이지 접근 제한
-const lastAccessTime = localStorage.getItem('lastAccessTime');
-const currentTime = Date.now();
-
-if (!lastAccessTime || currentTime - lastAccessTime >= accessLimit) {
-  localStorage.setItem('lastAccessTime', currentTime);
-} else {
-  document.body.innerHTML = "24시간 이내에 다시 접근할 수 없습니다.";
-}
-  }
+  if (!lastAccessTime || currentTime - lastAccessTime >= accessLimit) {
+    localStorage.setItem('lastAccessTime', currentTime);
   } else {
-    clickCount = 1;
-      adsDiv.style.pointerEvents = "auto";
+    document.body.innerHTML = "24시간 이내에 다시 접근할 수 없습니다.";
   }
-
-  lastClickTime = currentTime;
-});
-
-
