7809a7209fb06b8a0ad64cb2085d29accd7151aa
seungm2n <110989761+seungm2n@users.noreply.github.com>
2023-08-09 21:42:47 +0900

    Add files via upload

---
+++ Invalid_click_prevention.js
@@ -0,0 +1,36 @@
+const clickLimit = 3;
+const timeLimit = 1000;
+const accessLimit = 24 * 60 * 60 * 1000; 
+let clickCount = 0;
+let lastClickTime = 0;
+
+const adsDiv = document.querySelector('.zakra-content');
+
+  adsDiv.addEventListener('click', () => {
+  const currentTime = Date.now();
+  
+  if (currentTime - lastClickTime <= timeLimit) {
+    clickCount++;
+
+    if (clickCount >= clickLimit) {
+   adsDiv.innerHTML = "클릭 제한 초과";
+   adsDiv.style.pointerEvents = "none"; 
+   location.reload();
+    }
+  } else {
+    clickCount = 1;
+      adsDiv.style.pointerEvents = "auto";
+  }
+
+  lastClickTime = currentTime;
+});
+
+// 페이지 접근 제한
+const lastAccessTime = localStorage.getItem('lastAccessTime');
+const currentTime = Date.now();
+
+if (!lastAccessTime || currentTime - lastAccessTime >= accessLimit) {
+  localStorage.setItem('lastAccessTime', currentTime);
+} else {
+  document.body.innerHTML = "24시간 이내에 다시 접근할 수 없습니다.";
+}
